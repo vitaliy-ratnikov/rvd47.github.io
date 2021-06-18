93aaae37a10111611fa3f9975aa3d28a87ecd032
vitaly <vitaly.ratnikov747@gmail.com>
2021-06-18 18:11:18 +0300

    add event close outside modal

--- scripts.js
+++ scripts.js
@@ -1,13 +1,35 @@
 const modalBtn = document.querySelector('.button');
 const modalWindow = document.querySelector('.modal');
 const modalClose = document.querySelector('.modal-close');
+const modalWrap = document.querySelector('.modal-wrap');
 
 modalBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
-  modalWindow.classList.add('active');
+  modalWrap.classList.add('active');
 })
 
 modalClose.addEventListener('click', function (evt) {
   evt.preventDefault();
-  modalWindow.classList.remove('active');
+  modalWrap.classList.remove('active');
 })
+
+document.addEventListener('click' , function(evt){
+  if (!$(evt.target).closest(modalWindow, modalBtn).length) {
+    document.querySelector('body').find(modalWindow).removeClass("active");
+  }
+})
+
+modalWrap.addEventListener("click", function (event) {
+const isOutside = !event.target.closest(".modal");
+if (isOutside) {
+  modalWrap.classList.remove('active');
+
+}
+});
+
+window.addEventListener("keydown", (event) => {
+if (event.key === "Escape") {
+  modalWrap.classList.remove('active');
+
+}
+});
